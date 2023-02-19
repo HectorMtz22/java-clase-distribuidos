e2e3321788dd579773e511f34c8f4195cca6ac87
Hector Martinez <hec.gre81@gmail.com>
2023-02-19 03:11:10 -0600

    Add a log for the preffered thread and the time that lasted

--- distribuidos/src/act1/Main.java
+++ distribuidos/src/act1/Main.java
@@ -10,11 +10,20 @@ public class Main {
 		// TODO Auto-generated method stub
 		int hilos = 50;
 		long[] time = new long[hilos];
+		int hiloPreferido = -1;
+		long minTime = Long.MAX_VALUE;
 		Lanzador[] lanzadores = new Lanzador[hilos];
 		for (int i = 0; i < hilos; i++) {
 			lanzadores[i] = new Lanzador(i + 1, numeros, length);
 			time[i] = lanzadores[i].getTime();
+			if (time[i] < minTime) {
+				minTime = time[i];
+				hiloPreferido = i + 1;
+			}
 		}
+		
+		System.out.println("La cantidad de hilos que tardó menos fue: " + hiloPreferido);
+		System.out.println("con: " + minTime + " nanoseg.");
 	}
 
 }
