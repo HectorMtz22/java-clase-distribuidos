faae810ee33faef5c1d9eb7866a11643a0f97821
Hector Martinez <hec.gre81@gmail.com>
2023-03-14 19:36:36 -0600

    Initial project

---
+++ distribuidos/src/mc/Main.java
@@ -0,0 +1,14 @@
+package mc;
+
+public class Main {
+	int ANTS = 8;
+	int CAR_CAPACITY = 100;
+	int N_CARS = 3;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
